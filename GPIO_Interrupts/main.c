a2b16f251b53a83284b1dae53a65dba690dfa842
ananas-csun <ananas.ece25@gmail.com>
2025-02-08 15:22:34 -0800

    Added template for the GPIO Interrupts lab

---
+++ GPIO_Interrupts/main.c
@@ -0,0 +1,23 @@
+/**
+ * @file main.c
+ *
+ * @brief Main source code for the GPIO_Interrupts program.
+ *
+ * This file contains the main entry point and function definitions for the GPIO_Interrupts program.
+ * It interfaces with the following:
+ *  - User LED (RGB) Tiva C Series TM4C123G LaunchPad
+ *	- EduBase Board LEDs (LED0 - LED3)
+ *	- EduBase Board Push Buttons (SW2 - SW5)
+ *  - PMOD BTN (4 User Push Buttons)
+ *
+ * @author
+ */
+
+int main(void)
+{
+	
+	while(1)
+	{
+
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# ECE425L_GPIO_Interrupts
+# ECE 425L - Microprocessor Systems Lab
+**CSU Northridge**
+
+**Department of Electrical and Computer Engineering**
+
+## GPIO Interrupts Lab
+The GPIO lab interfaces with the following:
+- RGB LED of the TM4C123G LaunchPad
+- EduBase Board Push Buttons (SW2 - SW5)
+- EduBase Board LEDs (LED0 - LED3)
+- PMOD BTN - [Product Page](https://digilent.com/reference/pmod/pmodbtn/start)
+
